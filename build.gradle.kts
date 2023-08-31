0c16ee7b10d3cd1127d005b9c3572f24a9731e90
mike <maballesteros@gmail.com>
2023-08-31 16:21:01 +0200

    FIX publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    `maven-publish`
 }
-
-group = "com.koanly"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -21,9 +19,27 @@ kotlin {
     jvmToolchain(11)
 }
 
+allprojects {
+    version = "1.0-SNAPSHOT"
+    group = "com.github.gokoan"
+}
+
 subprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "maven-publish")
     repositories {
         mavenCentral()
+    }
+
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["kotlin"])
+            }
+        }
     }
 }
